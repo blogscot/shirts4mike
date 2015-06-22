137cb783218e78318fd5d42118f963c027cdfc15
blogscot <iain.diamond@gmail.com>
2015-06-22 14:39:25 +0100

    Improve error messages

--- contact.php
+++ contact.php
@@ -7,22 +7,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $message = trim($_POST["message"]);
 
   if ($name == "" or $email == "" or $message == "") {
-    echo "You must specify a value for a name, email address and message.";
-    exit;
+    $error_message = "You must specify a value for a name, email address and message.";
   }
 
-  # Prevent PHP Email header injection SNIPPET
-  foreach( $_POST as $value) {
-    if ( stripos($value, 'Content-Type:') != FALSE) {
-      echo "There was a problem with the information you entered.";
-      exit;
+  if (!isset($error_message)) {
+    # Prevent PHP Email header injection SNIPPET
+    foreach( $_POST as $value) {
+      if ( stripos($value, 'Content-Type:') != FALSE) {
+        $error_message = "There was a problem with the information you entered.";
+      }
     }
   }
 
   # Prevent spam bots
-  if ($_POST['address'] != "") {
-    echo "Your form submission has an error.";
-    exit;
+  if (!isset($error_message) and $_POST['address'] != "") {
+    $error_message = "Your form submission has an error.";
   }
 
   date_default_timezone_set('Europe/Paris');
@@ -30,52 +29,50 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   $mail = new PHPMailer();
 
-  if (!$mail->ValidateAddress($email)) {
-    echo "You must specify a valid email address.";
-    exit;
+  if (!isset($error_message) and !$mail->ValidateAddress($email)) {
+    $error_message = "You must specify a valid email address.";
   }
 
-$mail->isSMTP();
+  if (!isset($error_message)) {
+    $mail->isSMTP();
 
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Ask for HTML-friendly debug output
-$mail->Debugoutput = 'html';
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "iainDOTdiamondATgmailDOTcom";
-//Password to use for SMTP authentication
-$mail->Password = "thisisnotmypasswordnoreallyimnotjoking";
-//Set who the message is to be sent from
-$mail->setFrom($email, $name);
-//Set who the message is to be sent to
-$mail->addAddress('iainDOTdiamondATgmailDOTcom', 'Iain Diamond');
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML($message);
+    //Enable SMTP debugging
+    // 0 = off (for production use)
+    // 1 = client messages
+    // 2 = client and server messages
+    $mail->SMTPDebug = 2;
+    //Ask for HTML-friendly debug output
+    $mail->Debugoutput = 'html';
+    //Set the hostname of the mail server
+    $mail->Host = 'smtp.gmail.com';
+    //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+    $mail->Port = 587;
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Whether to use SMTP authentication
+    $mail->SMTPAuth = true;
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = "iainDOTdiamondATgmailDOTcom";
+    //Password to use for SMTP authentication
+    $mail->Password = "thisisnotmypasswordnoreallyimnotjoking";
+    //Set who the message is to be sent from
+    $mail->setFrom($email, $name);
+    //Set who the message is to be sent to
+    $mail->addAddress('iainDOTdiamondATgmailDOTcom', 'Iain Diamond');
+    //Set the subject line
+    $mail->Subject = 'PHPMailer GMail SMTP test';
+    //Read an HTML message body from an external file, convert referenced images to embedded,
+    //convert HTML into a basic plain-text alternative body
+    $mail->msgHTML($message);
 
-//send the message, check for errors
-// if (!$mail->send()) {
-//     echo "Mailer Error: " . $mail->ErrorInfo;
-//     exit;
-// }
-
-  # We'll learn to send an email in the future!
-
-  header("Location: contact.php?status=thanks");
-  exit;
+    // send the message, check for errors
+    // if ($mail->send()) {
+    //   header("Location: contact.php?status=thanks");
+    //   exit;
+    // } else {
+    //   $error_message = "Mailer Error: ". $mail->ErrorInfo;
+    // }
+  }
 }
 
 $pageTitle = "Contact";
@@ -91,15 +88,20 @@ include('includes/header.php'); ?>
   <?php if (isset($_GET["status"]) and $_GET["status"] == "thanks") { ?>
       <p>Thanks for the email I&rsquo;ll be in touch shortly.</p>
   <?php } else { ?>
-    <p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>
-    
+
+    <?php if (!isset($error_message)) {
+      echo '<p>I&rsquo;d love to hear from you! Complete the form to send me an email.</p>';
+    } else {
+      echo '<p class="message">'. $error_message .'</p>';
+      } ?>
+
     <form action="contact.php" method="POST">
       <label for="name">Name</label>
-      <input id="name" type="text" name="name" placeholder="Enter your name"><br>
+      <input id="name" type="text" name="name" value="<?php if (isset($name)) { echo htmlspecialchars($name); } ?>" placeholder="Enter your name"><br>
       <label for="email">Email</label>
-      <input id="email" type="text" name="email" placeholder="Enter your email address"><br>
+      <input id="email" type="text" name="email" value="<?php if (isset($email)) { echo htmlspecialchars($email); } ?>" placeholder="Enter your email address"><br>
       <label for="message">Message</label>
-      <textarea name="message" id="message" rows="10"></textarea><br>
+      <textarea name="message" id="message" rows="10"><?php if (isset($message)) { echo htmlspecialchars($message); } ?></textarea><br>
       <input type="submit" value="Send">
       <div style="display: none;">
         <label for="address">Address</label>
